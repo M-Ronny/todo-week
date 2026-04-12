75201bde9e15e54fa24ea1b9ad554a53e00a3d64
Ronaldo <ronaldomorales.ai@gmail.com>
2026-04-11 20:33:49 -0400

    Added event listener to complete with a function of incomplete to mark to do item back to incomplete

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
-const todoComplete = document.querySelectorAll('.todo')
+const todoComplete = document.querySelectorAll('.not')
 const deleteBtn = document.querySelectorAll('.delete')
+const todoIncomplete = document.querySelectorAll('.complete')
 
 Array.from(todoComplete).forEach(element => {
     element.addEventListener('click', complete)
@@ -9,19 +10,19 @@ Array.from(deleteBtn).forEach(element => {
     element.addEventListener('click', deleteTodo)
 }) 
 
+Array.from(todoIncomplete).forEach(element => {
+    element.addEventListener('click', incomplete)
+})
+
 async function complete() {
-    const item = this.textContent
-    const day = this.classList[1]
-    // const id = this.dataset.id
+    const id = this.dataset.id
 
     try {
         const response = await fetch('markComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                'itemFromJS': item,
-                'dayFromJS': day
-                // 'idFromJS': id
+                'idFromJS': id
             })
         })
         const data = await response.json()
@@ -38,6 +39,25 @@ async function deleteTodo() {
     try {
         const response = await fetch('deleteTodo', {
             method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'idFromJS': id
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+async function incomplete() {
+    const id = this.dataset.id
+
+    try {
+        const response = await fetch('incompleteTodo', {
+            method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 'idFromJS': id
